20ad049653e30195826a73c577526dfbc709f9f6
andSLASH <zaharov.andrey.93@gmail.com>
2026-04-10 12:11:07 +0300

    'fix2'

--- app/main.py
+++ app/main.py
@@ -37,7 +37,10 @@ class AndroidDeveloper(SoftwareEngineer):
         return "Ads every three swipes"
 
 
-class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
+class FullStackDeveloper(
+    FrontendDeveloper,
+    BackendDeveloper
+):
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
